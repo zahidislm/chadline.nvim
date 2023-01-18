55f892cc553d3e66d9553cb61e65696beb7fc28b
Zahid Islam <dev@zahidislm.com>
2023-01-18 00:57:55 -0500

    fix(config): Allow custom configuration.
    - fixed user-set config not applying/overridding default options.

--- lua/chadline/init.lua
+++ lua/chadline/init.lua
@@ -1,11 +1,18 @@
+local default_config = {
+	statusline = {
+		separator_style = "default", -- default/round/block/arrow
+		overriden_modules = nil,
+	},
+}
+
 local M = {}
-local config = require("chadline.config")
 
 M.statusline = function()
-	return require("chadline.components").run(config.statusline)
+	return require("chadline.components").run(M.user_config)
 end
 
-M.setup = function()
+M.setup = function(opts)
+	M.user_config = vim.tbl_deep_extend("force", {}, default_config, opts or {})
 	vim.opt.statusline = "%!v:lua.require('chadline').statusline()"
 end
 
